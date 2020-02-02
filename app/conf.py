a4ed6bcc4099df4a448e2ed9edcb17c8994ce4d4
brown <wangjiang@datagrand.com>
2020-02-02 17:14:00 +0800

    使用js完成微博页的渲染

--- app/conf.py
+++ app/conf.py
@@ -1,4 +1,4 @@
 import os
 
-posts_url = os.environ.get('POSTS_URL', 'http://127.0.0.1:8000/api/v1/posts')
-focuses_url = os.environ.get('FOCUSES_URL', 'http://127.0.0.1:8000/api/v1/focuses')
+posts_url = os.environ.get('POSTS_URL', 'http://47.103.153.69:8000/api/v1/posts')
+focuses_url = os.environ.get('FOCUSES_URL', 'http://47.103.153.69:8000/api/v1/focuses')
---
+++ app/static/js/posts.js
@@ -0,0 +1,19 @@
+// 处理微博列表相关
+
+function render_posts(data) {
+    // gen inner html
+    let item_template = "" +
+        "<a class=\"list-group-item\" href=\"https://m.weibo.cn/detail/{0}\">\n" +
+        "    <img src=\"../static/images/weibo/{1}\" alt=\"微博\" class=\"img-thumbnail\">\n" +
+        "</a>";
+    let items = [];
+    for (let i in data['result']['rock']) {
+        i = parseInt(i);
+        let item_data = data['result']['rock'][i];
+        let item_html = item_template.replace('{0}', item_data['id']).replace('{1}', item_data['image']);
+        items.push(item_html);
+    }
+
+    // replace
+    document.getElementById('weibo_list').innerHTML = items.join('\n');
+}
