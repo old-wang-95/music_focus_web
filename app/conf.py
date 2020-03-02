f76fb3a628a5cb5e4cfb78037502a63b95d8c089
brown <wangjiang@datagrand.com>
2020-03-02 19:02:03 +0800

    更改默认配置为服务名

--- app/conf.py
+++ app/conf.py
@@ -2,5 +2,5 @@ import os
 
 is_debug = True if os.environ.get('DEBUG', 'true').lower() == 'true' else False
 
-posts_url = os.environ.get('POSTS_URL', 'http://47.103.153.69:8000/api/v1/posts')
-focuses_url = os.environ.get('FOCUSES_URL', 'http://47.103.153.69:8000/api/v1/focuses')
+posts_url = os.environ.get('POSTS_URL', 'http://music_focus_online:8000/api/v1/posts')
+focuses_url = os.environ.get('FOCUSES_URL', 'http://music_focus_online:8000/api/v1/focuses')
