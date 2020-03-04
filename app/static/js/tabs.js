a88f6229b74c2adfd24d9425a5de560b1a001e93
brown <wangjiang@datagrand.com>
2020-03-04 20:32:01 +0800

    更新tabs.js

--- app/static/js/tabs.js
+++ app/static/js/tabs.js
@@ -1,17 +1,27 @@
 // 处理标签栏相关
 
 function load_tabs() {
-    $("#tabs").load("/static/html/tabs.html", set_biz_tabs_active);
+    $("#tabs").load("/static/html/tabs.html", function () {
+            const html_name = window.location.pathname.split("/").pop().split("?")[0];
+
+            // set biz tabs
+            if (html_name === "weibo.html") {
+                document.getElementById("biz_tabs").getElementsByTagName("li").item(0).classList.add("active");
+            } else if (html_name === "focus.html") {
+                document.getElementById("biz_tabs").getElementsByTagName("li").item(1).classList.add("active");
+            } else if (html_name === "lizhi.html" || html_name === "album.html") {
+                document.getElementById("biz_tabs").innerHTML = '';
+            }
+            // set music tabs
+            if (html_name === "lizhi.html" || html_name === "album.html") {
+                document.getElementById("music_tabs").getElementsByTagName("li").item(3).classList.add("active");
+            } else {
+                document.getElementById("music_tabs").getElementsByTagName("li").item(0).classList.add("active");
+            }
+        }
+    );
 }
 
-function set_biz_tabs_active() {
-    const html_name = window.location.pathname.split("/").pop();
-    if (html_name === "weibo.html") {
-        document.getElementById("biz_tabs").getElementsByTagName("li").item(0).classList.add("active");
-    } else if (html_name === "focus.html") {
-        document.getElementById("biz_tabs").getElementsByTagName("li").item(1).classList.add("active");
-    }
-}
 
 function click_music_type(cur_obj) {
     const music_type = cur_obj.getElementsByTagName('a').item(0).textContent;
