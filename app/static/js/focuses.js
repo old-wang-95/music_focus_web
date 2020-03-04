fc8bda0d8d698191be7a2bfb8cbaa888031b5070
brown <wangjiang@datagrand.com>
2020-03-04 22:54:14 +0800

    重构focuses.js

--- app/static/js/focuses.js
+++ app/static/js/focuses.js
@@ -1,48 +1,28 @@
 // 处理热点列表相关
 
 function render_focuses(data) {
-    // gen inner html
-    let top_item_template = "" +
-        "<a class=\"list-group-item\" href=\"{link}\">\n" +
-        "    <h5 class=\"text-left\">\n" +
-        "        <span class=\"no-top\">{index}</span>\n" +
-        "            &nbsp\n" +
-        "        <span class=\"focus\">{title}</span>\n" +
-        "            &nbsp\n" +
-        "        <span class=\"hot\">{hot}</span>\n" +
-        "    </h5>\n" +
-        "    <h5 class=\"text-right\">\n" +
-        "        <span class=\"users\">{users}</span>\n" +
-        "    </h5>\n" +
-        "</a>";
-    let item_template = "" +
-        "<a class=\"list-group-item\" href=\"{link}\">\n" +
-        "    <h5 class=\"text-left\">\n" +
-        "        <span class=\"no\">{index}</span>\n" +
-        "            &nbsp\n" +
-        "        <span class=\"focus\">{title}</span>\n" +
-        "            &nbsp\n" +
-        "        <span class=\"hot\">{hot}</span>\n" +
-        "    </h5>\n" +
-        "    <h5 class=\"text-right\">\n" +
-        "        <span class=\"users\">{users}</span>\n" +
-        "    </h5>\n" +
-        "</a>";
-    let items = [];
-    for (let i in data['result']['rock']) {
-        i = parseInt(i);
-        let item_data = data['result']['rock'][i];
-        let item_html;
-        console.log(i+1);
-        if (i + 1 <= 3) {
-            item_html = top_item_template.replace('{link}', item_data['link']).replace('{index}', (i + 1).toString()).replace('{title}', item_data['title']).replace('{hot}', item_data['recent_read'].toString()).replace('{users}', item_data['related_users'].join(', '));
-        } else {
-            item_html = item_template.replace('{link}', item_data['link']).replace('{index}', (i + 1).toString()).replace('{title}', item_data['title']).replace('{hot}', item_data['recent_read'].toString()).replace('{users}', item_data['related_users'].join(',&nbsp'));
-        }
+    $.get("/static/templates/top_focus.html", function (top_focus_template) {
+        $.get("/static/templates/focus.html", function (focus_template) {
+            let focus_html_list = [];
+            jQuery.each(data['result']['rock'], function (i, focus_data) {
+                let focus_html;
+                if (i + 1 <= 3) {
+                    focus_html = template2html(top_focus_template, focus_data, i);
+                } else {
+                    focus_html = template2html(focus_template, focus_data, i);
+                }
+                focus_html_list.push(focus_html);
+            });
+            document.getElementById('focus_list').innerHTML = focus_html_list.join('\n');
+        });
+    });
+}
 
-        items.push(item_html);
-    }
-
-    // replace
-    document.getElementById('focus_list').innerHTML = items.join('\n');
+function template2html(template, data, i) {
+    return template
+        .replace('{link}', data['link'])
+        .replace('{index}', (i + 1).toString())
+        .replace('{title}', data['title'])
+        .replace('{hot}', data['recent_read'].toString())
+        .replace('{users}', data['related_users'].join(', '));
 }
