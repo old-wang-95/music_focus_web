b2043ffc4e66f9788d7ea31c42eaca8e5941e15e
brown <wangjiang@datagrand.com>
2020-03-05 00:11:13 +0800

    删除common.json中的http_get函数, 直接使用jquery的getJSON函数在每个页面内部

--- app/static/js/focuses.js
+++ app/static/js/focuses.js
@@ -1,19 +1,21 @@
 // 处理热点列表相关
 
-function render_focuses(data) {
-    $.get("/static/templates/top_focus_item.html", function (top_focus_template) {
-        $.get("/static/templates/focus_item.html", function (focus_template) {
-            let focus_html_list = [];
-            jQuery.each(data['result']['rock'], function (i, focus_data) {
-                let focus_html;
-                if (i + 1 <= 3) {
-                    focus_html = template2html(top_focus_template, focus_data, i);
-                } else {
-                    focus_html = template2html(focus_template, focus_data, i);
-                }
-                focus_html_list.push(focus_html);
+function render_focuses() {
+    $.getJSON("/api/v1/focuses", function (data) {
+        $.get("/static/templates/top_focus_item.html", function (top_focus_template) {
+            $.get("/static/templates/focus_item.html", function (focus_template) {
+                let focus_html_list = [];
+                jQuery.each(data['result']['rock'], function (i, focus_data) {
+                    let focus_html;
+                    if (i + 1 <= 3) {
+                        focus_html = template2html(top_focus_template, focus_data, i);
+                    } else {
+                        focus_html = template2html(focus_template, focus_data, i);
+                    }
+                    focus_html_list.push(focus_html);
+                });
+                document.getElementById('focus_list').innerHTML = focus_html_list.join('\n');
             });
-            document.getElementById('focus_list').innerHTML = focus_html_list.join('\n');
         });
     });
 }
