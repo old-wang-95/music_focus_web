c1e25fe632254f76046a29aa7c6f3df6b62db9ff
brown <wangjiang@datagrand.com>
2020-03-11 19:57:41 +0800

    为底部按钮添加点击事件, 并完成分享功能

--- app/static/js/bottom_buttons.js
+++ app/static/js/bottom_buttons.js
@@ -1,3 +1,21 @@
 function load_bottom() {
     $('#bottom_buttons').load('/static/templates/bottom_buttons.html');
 }
+
+function share() {
+    $.get('/static/templates/share_mask.html', function (share_mask_html) {
+        $("body").prepend(share_mask_html);
+    });
+}
+
+function dismiss_share() {
+    $(".share_mask").remove();
+}
+
+function feedback() {
+
+}
+
+function subscribe() {
+
+}
