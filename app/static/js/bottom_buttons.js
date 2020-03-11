59a73d6f7d22564a8a716a51de8d4b0b4e3117d2
brown <wangjiang@datagrand.com>
2020-03-12 01:22:17 +0800

    完成反馈功能前端界面

--- app/static/js/bottom_buttons.js
+++ app/static/js/bottom_buttons.js
@@ -1,7 +1,10 @@
 function load_bottom() {
     $('#bottom_buttons').load('/static/templates/bottom_buttons.html');
+    register_feedback();
 }
 
+
+/* share相关 */
 function share() {
     $.get('/static/templates/share_mask.html', function (share_mask_html) {
         $("body").append(share_mask_html);
@@ -12,10 +15,29 @@ function dismiss_share() {
     $(".share_mask").remove();
 }
 
-function feedback() {
 
+/* feedback相关 */
+function register_feedback() {
+    $.get('/static/templates/feedback.html', function (feedback_html) {
+        $("body").append(feedback_html);
+        $('#feedback').on('show.bs.modal', function () {
+            let $this = $(this);
+            let $modal_dialog = $this.find('.modal-dialog');
+            $this.css('display', 'block');
+            $modal_dialog.css({'margin-top': Math.max(0, ($(window).height() - $modal_dialog.height()) / 2)});
+
+        });
+    })
 }
 
+function submit_feedback() {
+    let wx_id = $("#wx_id")[0].value;
+    let feedback_content = $("#feedback_content")[0].value;
+    //TODO
+    $("#feedback").modal("hide");
+}
+
+/* subscribe相关 */
 function subscribe() {
 
 }
