542a6748ef18c6d7b96dfcb110199b41662cd15c
brown <wangjiang@datagrand.com>
2020-04-29 23:50:12 +0800

    用cookie保存浏览位置 保存浏览item的xpath

--- app/static/js/common.js
+++ app/static/js/common.js
@@ -20,3 +20,46 @@ function scroll_load(list_element, list_data, item_template, template_func) {
         }
     }
 }
+
+class ViewPosition {
+    constructor(cookie_prefix = '') {
+        this._cookie_key = cookie_prefix + '_view_position';
+    }
+
+    save(ele, expires_second) {
+        $.cookie(this._cookie_key, XpathUtil.get_xpath(ele), {expires: expires_second / 60 / 60 / 24})
+    }
+
+    get() {
+        return $.cookie(this._cookie_key) !== null ? XpathUtil.get_ele($.cookie(this._cookie_key)) : null
+    }
+
+    scroll_to() {
+        let ele = this.get();
+        if (ele !== null) $(ele)[0].scrollIntoView();
+    }
+
+}
+
+class XpathUtil {
+    static get_xpath(element) {
+        if (element.tagName === 'HTML')
+            return '/HTML[1]';
+        if (element === document.body)
+            return '/HTML[1]/BODY[1]';
+
+        let ix = 0;
+        let siblings = element.parentNode.childNodes;
+        for (let i = 0; i < siblings.length; i++) {
+            let sibling = siblings[i];
+            if (sibling === element)
+                return XpathUtil.get_xpath(element.parentNode) + '/' + element.tagName + '[' + (ix + 1) + ']';
+            if (sibling.nodeType === 1 && sibling.tagName === element.tagName)
+                ix++;
+        }
+    }
+
+    static get_ele(xpath) {
+        return document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    }
+}
